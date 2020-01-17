922acf9485a8f20f2499bce9a28ebef142af71cd
shibin-k <shibin-k@users.noreply.github.com>
2020-01-17 10:14:41 -0800

    Update merge_pr.py

--- .github/actions/auto_merge/merge_pr.py
+++ .github/actions/auto_merge/merge_pr.py
@@ -1,1 +1,14 @@
-print("Merging PR")
+print("Running script to mergr PR")
+
+def neutral_exit():
+  sys.exit(0)
+  
+if __name__ == '__main__':
+    github_token = os.environ["GITHUB_TOKEN"]
+    github_repository = os.environ["GITHUB_REPOSITORY"]
+
+    github_event_path = os.environ["GITHUB_EVENT_PATH"]
+    event_data = json.load(open(github_event_path))
+    
+    print("Event Data")
+    print event_data
